cc8e0a886392da647c988937e70a2b49c8f47b6f
WongElone <wongelone0908@gmail.com>
2023-06-08 10:08:55 +0800

    Modifications for deployment to AWS Elastic Beanstalk and Use AWS S3 for static storages.
    Downgrade Django and Python to fit AWS Elastic Beanstalk (Python 3.8 running on 64bit Amazon Linux 2/3.5.3).
    Modify settings.py and storage_backends.py for static storages on AWS S3.

--- online-course-platform-api/online_course_platform/custom/storage_backends.py
+++ online-course-platform-api/online_course_platform/custom/storage_backends.py
@@ -3,9 +3,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.core.files.storage import FileSystemStorage
 
 
-# class StaticStorage(S3Boto3Storage):
-#     location = 'static'
-#     default_acl = 'public-read'
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
 
 # class PublicMediaStorage(S3Boto3Storage):
 #     location = 'media'
--- online-course-platform-api/online_course_platform/online_course_platform/settings.py
+++ online-course-platform-api/online_course_platform/online_course_platform/settings.py
@@ -125,7 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 
 USE_S3 = os.getenv('USE_S3') == 'yes'
 
@@ -138,11 +142,11 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-    # # s3 static settings
-    # AWS_LOCATION = 'static'
-    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    # # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # STATICFILES_STORAGE = 'custom.storage_backends.StaticStorage'
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'custom.storage_backends.StaticStorage'
 
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
@@ -155,10 +159,6 @@ if USE_S3:
 else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
